7e84dfe2d956c0f321309902ac8c813c5154c137
Loren Lima Martins <loren.lima@take.net>
2024-06-21 12:38:02 -0300

    feat: add controller

---
+++ Api/Blip.Valores.API/Controllers/GithubReposController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Entities;
+using Models.Interfaces.Facades;
+
+namespace Blip.Valores.API.Controllers;
+
+public class GithubReposController(IGithubReposFacade githubReposFacade) : ControllerBase
+{
+    private const int ZERO_VALUE = 0;
+
+    private readonly IGithubReposFacade _githubReposFacade = githubReposFacade;
+
+    [HttpGet("latest-repos")]
+    public async Task<ActionResult<List<GithubRepos>>> GetGithubRepos()
+    {
+        var repos = await _githubReposFacade.GetGithubReposAsync();
+        if (repos.Count is ZERO_VALUE)
+        {
+            return NotFound();
+        }
+        return Ok(repos);
+    }
+}
