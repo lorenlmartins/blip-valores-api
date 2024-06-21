3ea7bea2c1b00d50354666dcd3b60d5b5c6eb545
Loren Lima Martins <loren.lima@take.net>
2024-06-21 12:38:29 -0300

    feat: add infrastructure

---
+++ Api/Infrastructure/WebApi/IGithubClient.cs
@@ -0,0 +1,11 @@
+﻿using Models.Entities;
+using RestEase;
+
+namespace Infrastructure.WebApi;
+
+[Header("User-Agent", "Valores.Take.API")]
+public interface IGithubClient
+{
+    [Get("/orgs/takenet/repos?sort=created&direction=asc&language=C%23")]
+    Task<List<GithubRepos>> GetGithubReposAsync();
+}
