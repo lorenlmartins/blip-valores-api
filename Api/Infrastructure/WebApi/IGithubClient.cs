50a1bf431440a8a88e4589dfbf4eb3e4dcf19901
Loren Lima Martins <loren.lima@take.net>
2024-06-21 13:46:27 -0300

    fix: remove unused test

--- Api/Infrastructure/WebApi/IGithubClient.cs
+++ Api/Infrastructure/WebApi/IGithubClient.cs
@@ -3,7 +3,7 @@ using RestEase;
 
 namespace Infrastructure.WebApi;
 
-[Header("User-Agent", "Valores.Take.API")]
+[Header("User-Agent", "Blip.Valores.API")]
 public interface IGithubClient
 {
     [Get("/orgs/takenet/repos?sort=created&direction=asc&language=C%23")]
