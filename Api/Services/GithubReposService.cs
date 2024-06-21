f685e778282ca98fddd263283390c493bf01ee4b
Loren Lima Martins <loren.lima@take.net>
2024-06-21 12:38:49 -0300

    feat: add services

---
+++ Api/Services/GithubReposService.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.WebApi;
+using Models.Entities;
+using Models.Interfaces.Services;
+
+namespace Services;
+
+public class GithubReposService(IGithubClient githubClient) : IGithubReposService
+{
+    private const string CSHARP_LANGUAGE = "C#";
+    private const int LATEST_FIVE_REPOS = 5;
+    private const int ZERO_VALUE = 0;
+
+    private readonly IGithubClient _githubClient = githubClient;
+
+    public async Task<List<GithubRepos>> GetGithubReposAsync()
+    {
+        var repos = await _githubClient.GetGithubReposAsync();
+        if (repos.Count is ZERO_VALUE)
+        {
+            return [];
+        }
+        return repos.Where(x => x.Language != null && x.Language.Equals(CSHARP_LANGUAGE))
+            .Take(LATEST_FIVE_REPOS)
+            .ToList();
+    }
+}
